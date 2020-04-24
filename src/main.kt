f74e4127d7b5e4a9b7c73d61f66d0b3a00803177
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2020-04-24 15:13:03 +0200

    cd..

--- src/main.kt
+++ src/main.kt
@@ -1,3 +1,12 @@
 fun main(){
-    println("napis")
+    var a = 1.0
+    var b = 5.0
+    var c = 2.0
+
+    println("Program rozwiązuje równanie kwadratowe w postaci: ax^2+bx+c=0")
+    println("Wprowadź a:")
+
+    a = readLine()!!.toDouble()
+
+    RownanieKwadratowe(a,b,c).wyswietlRozwiazania()
 }
