8a2d7a9499fc6f2771aec0115a33b9488c4a4398
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2020-05-08 14:54:39 +0200

    cd..

--- src/main.kt
+++ src/main.kt
@@ -1,26 +1,35 @@
 fun main(){
-    var a : Double?
-    var b : Double?
-    var c : Double?
-
-    a = null
+    var a : Double
+    var b : Double
+    var c : Double
 
     println("Program rozwiązuje równanie kwadratowe w postaci: ax^2+bx+c=0")
 
     print("Wprowadź a:")
-    a = readLine()?.toDouble()
+    a = readLine1(10.0)
 
     print("Wprowadź b:")
-    b = readLine()?.toDouble()
+    b = readLine1(10.0)
 
     print("Wprowadź c:")
-    c = readLine()?.toDouble()
-
-    var d: Double
-    d=a?:10.0
+    c = readLine1(10.0)
 
     val rownanie = RownanieKwadratowe(a,b,c)
 
-    //rownanie.wyswietlRownanie()
-    //rownanie.wyswietlRozwiazania()
+    rownanie.wyswietlRownanie()
+    rownanie.wyswietlRozwiazania()
 }
+
+fun readLine1(defaultValue: Double):Double{
+    var a: Double
+
+    try {
+        a = readLine()!!.toDouble()
+    }catch(e:NumberFormatException){
+        println("Użyto wartości domyślnej: ${defaultValue}")
+        a = defaultValue
+    }
+
+    return a
+}
+
