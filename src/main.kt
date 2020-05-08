da0ced85cb1dcf89d68a990dd4b7a3ee7dc00a19
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2020-05-08 14:45:50 +0200

    cd..

--- src/main.kt
+++ src/main.kt
@@ -1,21 +1,26 @@
 fun main(){
-    var a = 1.0
-    var b = 5.0
-    var c = 2.0
+    var a : Double?
+    var b : Double?
+    var c : Double?
+
+    a = null
 
     println("Program rozwiązuje równanie kwadratowe w postaci: ax^2+bx+c=0")
 
     print("Wprowadź a:")
-    a = readLine()!!.toDouble()
+    a = readLine()?.toDouble()
 
     print("Wprowadź b:")
-    b = readLine()!!.toDouble()
+    b = readLine()?.toDouble()
 
     print("Wprowadź c:")
-    c = readLine()!!.toDouble()
+    c = readLine()?.toDouble()
+
+    var d: Double
+    d=a?:10.0
 
     val rownanie = RownanieKwadratowe(a,b,c)
 
-    rownanie.wyswietlRownanie()
-    rownanie.wyswietlRozwiazania()
+    //rownanie.wyswietlRownanie()
+    //rownanie.wyswietlRozwiazania()
 }
